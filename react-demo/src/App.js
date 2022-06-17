bb41d6582ee4cd0101fa85675f8084e85ada64e9
reece-elder <reece.elder@qa.com>
2022-06-17 10:03:11 +0100

    React Project Structure

---
+++ react-demo/src/App.js
@@ -0,0 +1,12 @@
+import logo from './resources/logo.svg';
+import './css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2> Hello World! </h2>
+    </div>
+  );
+}
+
+export default App;
